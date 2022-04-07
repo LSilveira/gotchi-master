850046b8651c681e782877a6f08795bc2c21a70c
Luis Silveira <luisilveira@gmail.com>
2022-04-07 03:06:44 +0100

    first commit

---
+++ src/main/java/com/gotchi/master/GotchiMasterApplication.java
@@ -0,0 +1,13 @@
+package com.gotchi.master;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GotchiMasterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GotchiMasterApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gotchi/master/GotchiMasterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gotchi.master;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GotchiMasterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
